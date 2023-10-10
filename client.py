89ffbfb901e0da36e04d6ebda6a52eb76f7b59df
Aaron Lengyel <aaron_lengyel@ucalgary.ca>
2023-10-09 22:31:23 -0600

    client and server files

---
+++ client.py
@@ -0,0 +1,10 @@
+from lib import ships, board, faction
+import socketserver
+
+def main():
+    # 
+    ...
+
+
+if __name__ == '__main__':
+    main()
--- server.py
+++ server.py
@@ -1,8 +1,13 @@
 from lib import ships, board, faction
 import socketserver
+import socket
+
+HOST = "127.0.0.1"
+PORT = 5000
 
 def main():
-    # 
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
     ...
 
 
